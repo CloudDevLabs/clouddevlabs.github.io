2d49fedb7b5304a6abd104a3b89b8157f4c63b5e
Yaroslav Novytskyy <ynovytskyy@gmail.com>
2018-11-24 19:55:44 -0500

    init with work delivered by KEEF on 2017-01-27

---
+++ js/common.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+	var options = {
+ 	 	offset: 450
+	}
+	var header = new Headhesive('.header_menu_fixed', '.menu_inner_fixed', options);
+
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".menu_inner").slideToggle();
+	});
+
+	$(".toggle-mnu_fixed").click(function() {
+		$(this).toggleClass("on");
+		$(".menu_inner_fixed").slideToggle();
+	});
+
+	new WOW().init();
+
+	$(".menu_inner, .menu_inner_fixed, .footer_nav").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;		
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+
+
+
+});
+
+
+
+
