9cd6bbcf0728f257d4cbae42a406b48914c1c127
Yuri Krynytskyi <ykrynytskyi@clouddevlabs.io>
2019-03-03 21:09:28 +0200

    fixing header_menu

--- js/common.js
+++ js/common.js
@@ -2,9 +2,16 @@ $(document).ready(function () {
 
 	new WOW().init();
 
-	$(".toggle_menu, .inner_menu").click(function() {
-		$(".toggle_menu").toggleClass("on");
+	$(".toggle_menu").click(function() {
+		$(".toggle_menu").toggleClass("on")
 		$(".inner_menu").slideToggle();
+	});
+	
+	$(".inner_menu, .logo_menu").click(function() {
+		if($(".toggle_menu").css("display") === "block"){
+			$(".toggle_menu").removeClass("on")
+			$(".inner_menu").slideUp();
+		}
 	});
 
 	$(".header_menu").on("click","a", function (event) {
