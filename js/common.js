0da03efdb69462a838cf95d064b460d413127e18
Yuri Krynytskyi <ykrynytskyi@clouddevlabs.io>
2019-03-03 02:16:23 +0200

    refactoring header_menu

--- js/common.js
+++ js/common.js
@@ -1,38 +1,16 @@
 $(document).ready(function () {
 
-	var options = {
-		offset: 450	  
-	}
-	
-	var header = new Headhesive(".header_menu", options);
+	new WOW().init();
 
-	//var header = new Headhesive('.header_menu_fixed', '.inner_menu_fixed', options);
-
-	$(".toggle_menu").click(function() {
-		$(this).toggleClass("on");
+	$(".toggle_menu, .inner_menu").click(function() {
+		$(".toggle_menu").toggleClass("on");
 		$(".inner_menu").slideToggle();
 	});
 
-	// $(".toggle_menu_fixed").click(function() {
-	// 	$(this).toggleClass("on");
-	// 	$(".inner_menu_fixed").slideToggle();
-	// });
-
-	new WOW().init();
-
-	//$(".inner_menu, .inner_menu_fixed, .logo").on("click","a", function (event) {
-		
-	$(".inner_menu, .logo").on("click","a", function (event) {
+	$(".header_menu").on("click","a", function (event) {
 		event.preventDefault();
 		var id  = $(this).attr('href'),
 		top = $(id).offset().top;		
 		$('body,html').animate({scrollTop: top}, 1000);
 	});
-
-
-
 });
-
-
-
-
