bc35d19213ba963728a9b6620a23a7b370b5e6ba
Yuri Krynytskyi <ykrynytskyi@clouddevlabs.io>
2019-03-03 23:14:41 +0200

    add fixed position header_menu

--- js/common.js
+++ js/common.js
@@ -3,21 +3,35 @@ $(document).ready(function () {
 	new WOW().init();
 
 	$(".toggle_menu").click(function() {
-		$(".toggle_menu").toggleClass("on")
+		$(".toggle_menu").toggleClass("on");
 		$(".inner_menu").slideToggle();
 	});
 	
 	$(".inner_menu, .logo_menu").click(function() {
 		if($(".toggle_menu").css("display") === "block"){
-			$(".toggle_menu").removeClass("on")
+			$(".toggle_menu").removeClass("on");
 			$(".inner_menu").slideUp();
 		}
 	});
 
+	$(window).resize(function() {
+		if($(".toggle_menu").css("display") === "block"){
+			$(".toggle_menu").removeClass("on");
+			$(".inner_menu").slideUp();
+		}
+	});
+
+	// $(window).scroll(function() {
+	// 	if($(".toggle_menu").css("display") === "block"){
+	// 		$(".toggle_menu").removeClass("on");
+	// 		$(".inner_menu").slideUp();
+	// 	}
+	// });
+
 	$(".header_menu").on("click","a", function (event) {
 		event.preventDefault();
-		var id  = $(this).attr('href'),
-		top = $(id).offset().top;		
-		$('body,html').animate({scrollTop: top}, 1000);
+		var id  = $(this).attr('href');
+		var top = $(id).offset().top - 59;		
+		$("body, html").animate({scrollTop: top}, 1000);
 	});
 });
