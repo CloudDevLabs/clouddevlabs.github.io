642c5196f050d3b707571bb7a2e917786474174f
Yuri Krynytskyi <ykrynytskyi@clouddevlabs.io>
2019-03-04 23:42:17 +0200

    change  menu classes to ids

--- js/common.js
+++ js/common.js
@@ -2,26 +2,26 @@ $(document).ready(function () {
 
 	new WOW().init();
 
-	$(".toggle_menu").click(function() {
-		$(".toggle_menu").toggleClass("on");
-		$(".inner_menu").slideToggle();
+	$("#toggle_menu").click(function() {
+		$("#toggle_menu").toggleClass("on");
+		$("#inner_menu").slideToggle();
 	});
 	
-	$(".inner_menu, .logo_menu").click(function() {
-		if($(".toggle_menu").css("display") === "block"){
-			$(".toggle_menu").removeClass("on");
-			$(".inner_menu").slideUp();
+	$("#inner_menu, #logo_menu").click(function() {
+		if($("#toggle_menu").css("display") === "block"){
+			$("#toggle_menu").removeClass("on");
+			$("#inner_menu").slideUp();
 		}
 	});
 
 	$(window).resize(function() {
-		if($(".toggle_menu").css("display") === "block"){
-			$(".toggle_menu").removeClass("on");
-			$(".inner_menu").slideUp();
+		if($("#toggle_menu").css("display") === "block"){
+			$("#toggle_menu").removeClass("on");
+			$("#inner_menu").slideUp();
 		}
 	});
 
-	$(".header_menu").on("click","a", function (event) {
+	$("#header_menu").on("click","a", function (event) {
 		event.preventDefault();
 		var id  = $(this).attr('href');
 		var top = $(id).offset().top - 59;		
